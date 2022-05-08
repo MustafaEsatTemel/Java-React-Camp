568434aa78bd4b55627c025c705fce6dc112d5cb
Mustafa Esat Temel <esattemel130@gmail.com>
2022-05-08 15:38:51 +0300

    removed Day6HomeworkHRMS

--- Day6HomeworkHRMS/src/main/java/me/mustafaesattemel/hrms/dataAccess/abstracts/UserDao.java
+++
@@ -1,9 +0,0 @@
-package me.mustafaesattemel.hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import me.mustafaesattemel.hrms.entities.concretes.User;
-
-public interface UserDao extends JpaRepository<User, Integer> {
-
-}
